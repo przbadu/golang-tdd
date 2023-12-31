9bc8be1785ad5e97883e0a71b49044b86aa28302
Pushpa Raj Badu <prz.badu@gmail.com>
2023-12-31 20:51:18 +0545

    Handle the default name with test case

--- 02_tdd_hello_name/hello_test.go
+++ 02_tdd_hello_name/hello_test.go
@@ -3,10 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	actual := Hello("Chris")
-	expected := "Hello, Chris"
+	t.Run("saying hello to peope", func(t *testing.T) {
+		actual := Hello("Chris")
+		expected := "Hello, Chris"
 
-	if actual != expected {
-		t.Errorf("Expected %q, Actual: %q", expected, actual)
-	}
+		if actual != expected {
+			t.Errorf("Expected %q, Actual: %q", expected, actual)
+		}
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		actual := Hello("")
+		expected := "Hello, World"
+
+		if actual != expected {
+			t.Errorf("Expected %q, Actual: %q", expected, actual)
+		}
+	})
 }
