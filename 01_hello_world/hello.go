284ecf825e83777ade5f0e2886a9e1881d0ae1b7
Pushpa Raj Badu <prz.badu@gmail.com>
2023-12-31 20:05:10 +0545

    Separation of concern so that our code becomes easy to test.

--- 01_hello_world/hello.go
+++ 01_hello_world/hello.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+// Separate the concerns of the code
+// So that it is easier to test the code
+func Hello() string {
+	return "Hello, World"
+}
+
 func main() {
-	fmt.Println("Hello, world")
+	fmt.Println(Hello())
 }
