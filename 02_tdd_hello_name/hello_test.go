d03986bea9a8034f177e284e94c79031d28ef64f
Pushpa Raj Badu <prz.badu@gmail.com>
2023-12-31 21:09:41 +0545

    Add locale support Hello method

--- 02_tdd_hello_name/hello_test.go
+++ 02_tdd_hello_name/hello_test.go
@@ -4,21 +4,30 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to peope", func(t *testing.T) {
-		actual := Hello("Chris")
+		actual := Hello("Chris", "")
 		expected := "Hello, Chris"
 
 		assertCorrectMessage(t, actual, expected)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		actual := Hello("")
+		actual := Hello("", "")
 		expected := "Hello, World"
 
+		assertCorrectMessage(t, actual, expected)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		actual := Hello("Elodie", "es")
+		expected := "Hola, Elodie"
 		assertCorrectMessage(t, actual, expected)
 	})
 }
 
 func assertCorrectMessage(t testing.TB, actual, expected string) {
+	// making this a helper function, so that if test fails
+	// it displays line number from the actual function rather
+	// than from this helper function.
 	t.Helper()
 
 	if actual != expected {
--- 02_tdd_hello_name/main.go
+++ 02_tdd_hello_name/main.go
@@ -2,16 +2,24 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
+const (
+	spanish            = "es"
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+)
 
-func Hello(name string) string {
+func Hello(name string, lang string) string {
 	if name == "" {
 		name = "World"
+	}
+
+	if lang == spanish {
+		return spanishHelloPrefix + name
 	}
 
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("Chris"))
+	fmt.Println(Hello("Chris", ""))
 }
