36bff74722130fb48ae71fd9f8f34e65704f39d7
Pushpa Raj Badu <prz.badu@gmail.com>
2024-01-02 10:03:12 +0545

    Handle empty slice case

--- 06_array/main.go
+++ 06_array/main.go
@@ -17,6 +17,11 @@ func SumAll(numbersToSum ...[]int) (sums []int) {
 
 func SumAllTails(numbersToSum ...[]int) (sums []int) {
 	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+			continue
+		}
+
 		tail := numbers[1:]
 		sums = append(sums, Sum(tail))
 	}
