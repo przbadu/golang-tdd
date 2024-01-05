a66f294f56b3e7b6c43eb517090d635278251aab
Pushpa Raj Badu <prz.badu@gmail.com>
2024-01-05 10:40:08 +0545

    Add Rectangle struct to give clear definitation to the required arguments

--- 07-structs/perimeter_test.go
+++ 07-structs/perimeter_test.go
@@ -3,17 +3,23 @@ package main
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
 	expected := 40.0
 
-	if got != expected {
-		t.Errorf("Expected %.2f but got %.2f", expected, got)
-	}
+	assert(t, expected, got)
 }
 
 func TestArea(t *testing.T) {
-	got := Area(10.0, 10.0)
-	expected := 100.0
+	rectangle := Rectangle{12.0, 6.0}
+	got := Area(rectangle)
+	expected := 72.0
+
+	assert(t, expected, got)
+}
+
+func assert(t testing.TB, expected, got float64) {
+	t.Helper()
 
 	if got != expected {
 		t.Errorf("Expected %.2f but got %.2f", expected, got)
