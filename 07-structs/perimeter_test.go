c38a19e51e966c035f83d6766775c23ad5813c26
Pushpa Raj Badu <prz.badu@gmail.com>
2024-01-05 10:33:47 +0545

    add logic to calculate area of rectangle

--- 07-structs/perimeter_test.go
+++ 07-structs/perimeter_test.go
@@ -7,6 +7,15 @@ func TestPerimeter(t *testing.T) {
 	expected := 40.0
 
 	if got != expected {
-		t.Errorf("Expected %v but got %v", expected, got)
+		t.Errorf("Expected %.2f but got %.2f", expected, got)
+	}
+}
+
+func TestArea(t *testing.T) {
+	got := Area(10.0, 10.0)
+	expected := 100.0
+
+	if got != expected {
+		t.Errorf("Expected %.2f but got %.2f", expected, got)
 	}
 }
